70f9ac53427df8876a144845cf951f08b6900533
Eric Hameleers <alien@slackware.com>
2016-01-14 17:20:02 +0100

    VERSION bump from Beta 3 (aka 0.3) to 0.3.8.
    Roadmap:
    v0.3.9: adding LUKS encrypted /home
    v0.4 (Beta4): LUKS home setup stable enough for public consumption.

--- make_slackware_live.sh
+++ make_slackware_live.sh
@@ -35,7 +35,7 @@
 # -----------------------------------------------------------------------------
 
 # Version of the Live OS generator:
-VERSION="Beta3"
+VERSION="0.3.8"
 
 # Directory where our live tools are stored:
 LIVE_TOOLDIR=${LIVE_TOOLDIR:-"$(cd $(dirname $0); pwd)"}
