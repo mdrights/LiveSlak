4ebc965ba2dcb4dfe2d2cabe1bc80307eaa904cf
Eric Hameleers <alien@slackware.com>
2016-05-13 22:57:32 +0200

    Bump VERSION to 0.9.1.

--- make_slackware_live.sh
+++ make_slackware_live.sh
@@ -36,7 +36,7 @@
 # -----------------------------------------------------------------------------
 
 # Version of the Live OS generator:
-VERSION="0.9.0"
+VERSION="0.9.1"
 
 # Directory where our live tools are stored:
 LIVE_TOOLDIR=${LIVE_TOOLDIR:-"$(cd $(dirname $0); pwd)"}
