5fbc3d23064f61d215a8267f0e2bbeb5226ac0dc
Eric Hameleers <alien@slackware.com>
2016-06-03 15:12:58 +0200

    VERSION bump to 1.0.2.

--- make_slackware_live.sh
+++ make_slackware_live.sh
@@ -36,7 +36,7 @@
 # -----------------------------------------------------------------------------
 
 # Version of the Live OS generator:
-VERSION="1.0.1"
+VERSION="1.0.2"
 
 # Directory where our live tools are stored:
 LIVE_TOOLDIR=${LIVE_TOOLDIR:-"$(cd $(dirname $0); pwd)"}
