7e848953b075b72d43a19386e3a8ea10185e0ba9
Eric Hameleers <alien@slackware.com>
2016-08-09 13:33:29 +0200

    Live OS: enable CUPS network browsing by default.
    This makes remote CUPS printers available locally.

--- make_slackware_live.sh
+++ make_slackware_live.sh
@@ -1627,6 +1627,7 @@ sed -i ${LIVE_ROOTDIR}/etc/inittab -e "s/\(id:\).\(:initdefault:\)/\1${RUNLEVEL}
 # But enable NFS client support and CUPS:
 [ -f ${LIVE_ROOTDIR}/etc/rc.d/rc.rpc ] && chmod +x ${LIVE_ROOTDIR}/etc/rc.d/rc.rpc
 [ -f ${LIVE_ROOTDIR}/etc/rc.d/rc.cups ] && chmod +x ${LIVE_ROOTDIR}/etc/rc.d/rc.cups
+[ -f ${LIVE_ROOTDIR}/etc/rc.d/rc.cups-browsed ] && chmod +x ${LIVE_ROOTDIR}/etc/rc.d/rc.cups-browsed
 
 # Add a softvol pre-amp to ALSA - some computers have too low volumes.
 # If etc/asound.conf exists it's configuring ALSA to use Pulse,
