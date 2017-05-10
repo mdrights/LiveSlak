df060433480d44a4279574d219c958428a4c9df3
Eric Hameleers <alien@slackware.com>
2017-05-10 23:27:00 +0200

    Slackware Live Edition: version 1.1.8

--- make_slackware_live.sh
+++ make_slackware_live.sh
@@ -35,7 +35,7 @@
 # -----------------------------------------------------------------------------
 
 # Version of the Live OS generator:
-VERSION="1.1.7.1"
+VERSION="1.1.8"
 
 # Directory where our live tools are stored:
 LIVE_TOOLDIR=${LIVE_TOOLDIR:-"$(cd $(dirname $0); pwd)"}
