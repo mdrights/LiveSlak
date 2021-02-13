b9fff0fa51bd27f42dc3516e1f729764a545d904
mdrights <mdrights@tutanota.de>
2021-02-13 22:55:15 +0800

    let the version in useradd configurable in .conf file.

--- make_slackware_live.sh
+++ make_slackware_live.sh
@@ -1430,7 +1430,7 @@ EOL
 echo "root:${ROOTPW}" | chroot ${LIVE_ROOTDIR} /usr/sbin/chpasswd
 
 # Create a nonprivileged user account (called "live" by default):
-chroot ${LIVE_ROOTDIR} /usr/sbin/useradd -c "antiS 2021.01" -g users -G wheel,audio,cdrom,floppy,plugdev,video,power,netdev,lp,scanner,kmem,dialout,games,disk,input -u 1000 -d /home/${LIVEUID} -m -s /bin/zsh ${LIVEUID}
+chroot ${LIVE_ROOTDIR} /usr/sbin/useradd -c "${LIVEVER}" -g users -G wheel,audio,cdrom,floppy,plugdev,video,power,netdev,lp,scanner,kmem,dialout,games,disk,input -u 1000 -d /home/${LIVEUID} -m -s /bin/zsh ${LIVEUID}
 echo "${LIVEUID}:${LIVEPW}" | chroot ${LIVE_ROOTDIR} /usr/sbin/chpasswd
 
 # Configure suauth:
