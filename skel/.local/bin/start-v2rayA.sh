a1dac76613361cc1b85cb9af742a547afefc8463
mdrights <mdrights@tutanota.de>
2022-02-11 12:36:38 +0000

    have to remove v2raya.desktop since it does not work in live

--- skel/.local/bin/start-v2rayA.sh
+++ skel/.local/bin/start-v2rayA.sh
@@ -3,7 +3,7 @@
 # Try to start Docker daemon first whatever is it running
 sudo /etc/rc.d/rc.docker start || true
 
-sleep 3
+sleep 5
 
 docker run -d \
   -p 2017:2017 \
@@ -13,8 +13,12 @@ docker run -d \
   -v /etc/v2raya:/etc/v2raya \
   mzz2017/v2raya
 
+sleep 1
+
 xdg-open "http://127.0.0.1:2017"
 
 # Keep this terminal open for a while otherwise Firefox will not open.
 # If no browser is opened, please type `localhost:2017` in any browser.
 sleep 2
+
+exit
